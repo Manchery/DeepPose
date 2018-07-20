5c7b6e8f210ac3b0b2bfc129794586fc8eb8354c
Manchery <manchery@outlook.com>
2018-07-20 17:03:31 +0800

    modify deeppose_train.sh

--- deeppose_train.sh
+++ deeppose_train.sh
@@ -10,16 +10,14 @@ python ./data/K2HPD/get_stage1.py
 python ./models/deeppose/get_k2hpd.py
 ./models/deeppose/create_data_lmdb.sh
 ./models/deeppose/train.sh
-#python ./models/deeppose/pyaccuracy.py ./models/deeppose.caffemodel
-#python ./models/deeppose/pyoutput.py ./models/deeppose.caffemodel
 
-#python ./models/deeppose/pyaccuracy.py ./models/deeppose/snapshot/deeppose_iter_120000.caffemodel
-python ./models/deeppose/pyoutput.py ./models/deeppose/snapshot/deeppose_iter_120000.caffemodel
+python ./models/deeppose/pyaccuracy.py ./models/deeppose/snapshot/deeppose_iter_60000.caffemodel
+python ./models/deeppose/pyoutput.py ./models/deeppose/snapshot/deeppose_iter_60000.caffemodel
 
 python ./data/K2HPD/get_stage2.py
 
 python ./models/deeppose_refine/get_k2hpd2.py
 ./models/deeppose_refine/create_data_lmdb.sh
 ./models/deeppose_refine/train.sh
-#python ./models/deeppose_refine/pyaccuracy.py ./models/deeppose.caffemodel ./models/deeppose_refine.caffemodel
-#python ./models/deeppose_refine/pyaccuracy.py ./models/deeppose/snapshot/deeppose_iter_120000.caffemodel ./models/deeppose_refine/snapshot/deeppose_refine_iter_120000.caffemodel
+
+python ./models/deeppose_refine/pyaccuracy.py ./models/deeppose/snapshot/deeppose_iter_60000.caffemodel ./models/deeppose_refine/snapshot/deeppose_refine_iter_40000.caffemodel
